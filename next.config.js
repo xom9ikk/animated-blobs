8a4d39186cbb8cdc07fce720aaed946379843d1c
Max Romanyuta <xom9ik.code@gmail.com>
2021-01-02 11:23:46 +0200

    chore(pwa): add pwa for next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const WorkerPlugin = require("worker-plugin");
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-module.exports = withBundleAnalyzer({
+const app = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -18,4 +20,14 @@ module.exports = withBundleAnalyzer({
     }
     return config;
   },
-});
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+};
+
+module.exports = withBundleAnalyzer(
+  withPWA(
+    app
+  )
+)
